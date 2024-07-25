307b3c7e71da9f0a2ec237283641d9de97094f15
Karan <email@karanshergill.com>
2024-07-26 01:07:55 +0530

    desktop view fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Primary-Bright-Orange': 'hsl(31, 77%, 52%)',
+        'Primary-Dark-Cyan': 'hsl(184, 100%, 22%)',
+        'Primary-Very-Dark-Cyan': 'hsl(179, 100%, 13%)',
+        'Neutral-Transparent-White': 'hsla(0, 0%, 100%, 0.75)',
+        'Neutral-Very-Light-Gray': 'hsl(0, 0%, 95%)'
+      },
+      fontFamily: {
+        'Lexend-Deca': ["Lexend Deca", 'sans-serif'],
+        'Big-Shoulders': ["Big Shoulders Display", 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
